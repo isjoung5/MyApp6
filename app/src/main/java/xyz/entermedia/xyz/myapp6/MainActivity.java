d8ded23fd17fdaa042e8528ceb5ea8dc5562f931
isjoung5 <isjoung5@gmail.com>
2016-08-29 16:39:56 +0900

    8

--- app/src/main/java/xyz/entermedia/xyz/myapp6/MainActivity.java
+++ app/src/main/java/xyz/entermedia/xyz/myapp6/MainActivity.java
@@ -2,6 +2,7 @@ package xyz.entermedia.xyz.myapp6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private final long	FINSH_INTERVAL_TIME    = 1000;
+    private long		backPressedTime        = 0;
+
+    @Override
+    public void onBackPressed() {
+
+        long tempTime        = System.currentTimeMillis();
+        long intervalTime    = tempTime - backPressedTime;
+
+        if ( 0 <= intervalTime && FINSH_INTERVAL_TIME >= intervalTime ) {
+            //super.onBackPressed();
+            moveTaskToBack(true);
+            finish();
+        } else {
+            backPressedTime = tempTime;
+            Toast.makeText(getApplicationContext(),R.string.msg_004,Toast.LENGTH_SHORT).show();
+        }
     }
 }
